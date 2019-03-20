6f8b8731af8d0863dc45b959d4ec10589213147c
MacBookPro <mmfinfotect@MacBookPros-MacBook-Pro-2.local>
2019-03-20 15:15:49 +0530

    Initial Commit

---
+++ MKMapCluster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MKMapCluster
+//
+//  Created by MacBookPro on 29/12/1940 Saka.
+//  Copyright © 1940 mmfInfotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ MKMapClusterTests/MKMapClusterTests.swift
@@ -0,0 +1,34 @@
+//
+//  MKMapClusterTests.swift
+//  MKMapClusterTests
+//
+//  Created by MacBookPro on 29/12/1940 Saka.
+//  Copyright © 1940 mmfInfotech. All rights reserved.
+//
+
+import XCTest
+@testable import MKMapCluster
+
+class MKMapClusterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
